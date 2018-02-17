3571b54a0819dc80c9051d4bccc365e1f3f78105
hongyiweiwu <36486108+hongyiweiwu@users.noreply.github.com>
2018-02-17 00:59:59 -0500

    Add files via upload

--- Convertor.py
+++ Convertor.py
@@ -1,61 +1,71 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12. GPIO.OUT)
 
-# Break a string into characters. Convert unrecognizable character into Unrecognizable(R).
-def parser(incomingString):
-    for c in incomingString:
-        if not isConvertable(c):
-            c = '{'
 
-    return c
+class Convertor:
+    def _init_(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.setup(20, GPIO.OUT)
+        GPIO.setup(21, GPIO.OUT)
+        GPIO.setup(13, GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
+        GPIO.setup(26, GPIO.OUT)
 
-# Check if a character is convertable.
-def isConvertable(a):
-    return a in dict
+    # Break a string into characters. Convert unrecognizable character into Unrecognizable(R).
+    def parser(incomingString):
+        for c in incomingString:
+            if not isConvertable(c):
+                c = '{'
 
-# Convert a letter to a list of binary commands.
-def convertor(a):
-    return dict.get(a)
-#54, 56, 41, 77, 58, 59,
+        return c
 
-# Change an array of binary Braille dot commands to actual output commands.
-# GPIO 16 for left-upper; GPIO 20 for left-middle; GPIO 21 for left-down;
-# GPIO 13 for right-upper; GPIO 19 for right-middle; GPIO 26 for right-down.
-def output(booleanValues):
-    high = list()
-    low = list()
+    # Check if a character is convertable.
+    def isConvertable(a):
+        return a in dict
 
-    if booleanValues[0] == 1
-        high.append(16)
-    else
-        low.append(16)
+    # Convert a letter to a list of binary commands.
+    def convertor(a):
+        return dict.get(a)
+    #54, 56, 41, 77, 58, 59,
 
-    if booleanValues[1] == 1
-        high.append(20)
-    else
-        low.append(20)
+    # Change an array of binary Braille dot commands to actual output commands.
+    # GPIO 16 for left-upper; GPIO 20 for left-middle; GPIO 21 for left-down;
+    # GPIO 13 for right-upper; GPIO 19 for right-middle; GPIO 26 for right-down.
+    def output(booleanValues):
+        high = list()
+        low = list()
 
-    if booleanValues[2] == 1
-        high.append(21)
-    else
-        low.append(21)
+        if booleanValues[0] == 1
+            high.append(16)
+        else
+            low.append(16)
 
-    if booleanValues[3] == 1
-        high.append(13)
-    else
-        low.append(13)
+        if booleanValues[1] == 1
+            high.append(20)
+        else
+            low.append(20)
 
-    if booleanValues[4] == 1
-        high.append(19)
-    else
-        low.append(19)
+        if booleanValues[2] == 1
+            high.append(21)
+        else
+            low.append(21)
 
-    if booleanValues[5] == 1
-        high.append(26)
-    else
-        low.append(26)
+        if booleanValues[3] == 1
+            high.append(13)
+        else
+            low.append(13)
 
-    GPIO.output(high, 1)
-    GPIO.output(low, 0)
+        if booleanValues[4] == 1
+            high.append(19)
+        else
+            low.append(19)
 
+        if booleanValues[5] == 1
+            high.append(26)
+        else
+            low.append(26)
+
+        GPIO.output(high, 1)
+        GPIO.output(low, 0)
+
+        
