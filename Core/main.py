96fcc3f803931fd559e8c712e02587f87a760bdd
Yi Hong <yi@Yis-MacBook-Pro.local>
2018-02-17 16:37:41 -0500

    Remove useless features.

--- Core/main.py
+++ Core/main.py
@@ -1,12 +1,5 @@
 from Loader import FileLoader
 from Convertor import ENGLISH
-import time
-
-
-dateLastOpened = 0
-dateFile = open("DateLastOpened.txt", "r+")
-dateLastOpened = int(dateFile.read())
-dateFile.write(time.time())
 
 # Test code
 b = FileLoader("Text.txt")
