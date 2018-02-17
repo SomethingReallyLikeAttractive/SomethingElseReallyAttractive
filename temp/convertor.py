138f0f8726fb02968fc32a668602f36f06b27e1e
Yi Hong <yi@Yis-MacBook-Pro.local>
2018-02-17 00:12:15 -0500

    Thingy

---
+++ temp/convertor.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12. GPIO.OUT)
+
+# GPIO 16 for left-upper; GPIO 20 for left-middle; GPIO 21 for left-down;
+# GPIO 13 for right-upper; GPIO 19 for right-middle; GPIO 26 for right-down.
+
+def output(booleanValues):
+    high = list()
+    low = list()
+
+    if booleanValues[0] == True
+        high.append(16)
+    else
+        low.append(16)
+
+    if booleanValues[0] == True
+        high.append(16)
+    else
+        low.append(16)
+
+    if booleanValues[0] == True
+        high.append(16)
+    else
+        low.append(16)
+
+    if booleanValues[0] == True
+        high.append(16)
+    else
+        low.append(16)
+
+    if booleanValues[0] == True
+        high.append(16)
+    else
+        low.append(16)
+
+    if booleanValues[0] == True
+        high.append(16)
+    else
+        low.append(16)
