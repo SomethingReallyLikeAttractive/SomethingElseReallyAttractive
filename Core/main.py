7b122eaa7fcb0d8c7cf7a39eb18eb0600fe17fce
Yi Hong <yi@Yis-MacBook-Pro.local>
2018-02-17 19:18:22 -0500

    f

--- Core/main.py
+++ Core/main.py
@@ -12,14 +12,15 @@ GPIO.setup(BUTTONPIN, GPIO.IN)
 fileLoader = FileLoader('Text.txt')
 fileLoader.sendTextToRasp()
 
-while True:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTONPIN,GPIO.IN)
 
-    input()
-    result = subprocess.check_output('ls')
+#while True:
+    #GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(BUTTONPIN,GPIO.IN)
+
+    #input()
+    #result = subprocess.check_output('ls')
     #result = subprocess.check_output('fswebcam -r 4352x3264 --no-banner img.jpg && tesseract img.jpg Picwords.txt')
-    wordDetected = result.decode("utf-8")
-    detectedLoader = StringLoader(wordDetected)
-    detectedLoader.sendTextToRasp()
-    time.sleep(0.05)
+    #wordDetected = result.decode("utf-8")
+    #detectedLoader = StringLoader(wordDetected)
+    #detectedLoader.sendTextToRasp()
+    #time.sleep(0.05)
