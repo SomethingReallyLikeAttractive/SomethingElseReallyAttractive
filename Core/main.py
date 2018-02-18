1fc6fb6af866b3a77d62e8ea6889c700c9d5e77a
Yi Hong <yi@Yis-MacBook-Pro.local>
2018-02-17 20:09:50 -0500

    Add support for keyboard interrupting photo capture

--- Core/main.py
+++ Core/main.py
@@ -2,7 +2,6 @@ from Loader import FileLoader, StringLoader
 from Convertor import ENGLISH
 import RPi.GPIO as GPIO
 import time
-#import subprocess
 import os
 
 BUTTONPIN = 21
@@ -10,12 +9,15 @@ BUTTONPIN = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTONPIN, GPIO.IN)
 
-while True:
-    os.system('fswebcam -r 4352x3264 --no-banner img.jpg && tesseract img.jpg Picwords')
-    detectedLoader = FileLoader("Picwords.txt")
-    print("The following word is detected: ")
-    print(detectedLoader.returnText())
-    detectedLoader.sendTextToRasp()
+try:
+    while True:
+        os.system('fswebcam -r 4352x3264 --no-banner img.jpg && tesseract img.jpg Picwords')
+        detectedLoader = FileLoader("Picwords.txt")
+        print("The following word is detected: ")
+        print(detectedLoader.returnText())
+        detectedLoader.sendTextToRasp()
+except KeyboardInterrupt:
+    pass
 
 GPIO.output([14, 18, 23, 25, 7, 12], 0)
 os.system('rm Picwords.txt')
